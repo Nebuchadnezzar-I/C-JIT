aa83262922662ea971a655bce6fbf8b503b2af84
Nebuchadnezzar-I <onur.orrmenn@gmail.com>
2024-11-22 16:01:57 +0100

    Dump this

--- build/CMakeFiles/compiler.dir/main.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/compiler.dir/main.cpp.o: \
- /home/admiralcapo/personal/C-JIT/main.cpp /usr/include/stdc-predef.h
--- build/CMakeFiles/tests.dir/test/TokenTest.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/tests.dir/test/TokenTest.cpp.o: \
- /home/admiralcapo/personal/C-JIT/test/TokenTest.cpp \
- /usr/include/stdc-predef.h
